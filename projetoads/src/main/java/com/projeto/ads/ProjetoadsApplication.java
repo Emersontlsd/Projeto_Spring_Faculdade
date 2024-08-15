9cfb352a3b5852c2cf683230276f84a991a4d5f5
Emersontlsd <emersonsdom13@hotmail.com>
2024-08-15 09:40:26 -0300

    Initial project

---
+++ projetoads/src/main/java/com/projeto/ads/ProjetoadsApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.ads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoadsApplication.class, args);
+	}
+
+}
